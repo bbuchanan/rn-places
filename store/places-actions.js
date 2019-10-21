8363d8cf4212afa864f8c972159dfb72e33d66b7
Bill Buchanan <bbuchanan@gmail.com>
2019-10-21 13:53:03 -0400

    235. Fetching data from the local database

--- store/places-actions.js
+++ store/places-actions.js
@@ -1,8 +1,9 @@
 import * as FileSystem from "expo-file-system";
 
 export const ADD_PLACE = "ADD_PLACE";
+export const SET_PLACES = "SET_PLACES";
 
-import { insertPlace } from "../helpers/db";
+import { insertPlace, fetchPlaces } from "../helpers/db";
 
 export const addPlace = (title, image) => {
   return async dispatch => {
@@ -30,6 +31,17 @@ export const addPlace = (title, image) => {
       });
     } catch (err) {
       console.log(err);
+      throw err;
+    }
+  };
+};
+
+export const loadPlaces = () => {
+  return async dispatch => {
+    try {
+      const dbResult = await fetchPlaces();
+      dispatch({ type: SET_PLACES, places: dbResult.rows._array });
+    } catch (err) {
       throw err;
     }
   };
